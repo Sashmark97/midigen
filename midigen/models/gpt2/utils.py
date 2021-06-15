6c9a6b99caec65a4269697b2009c9f8b197393a2
Alex <sashmark97@gmail.com>
2021-06-15 10:01:05 +0300

    Added GPTConfig for GPT-2 initialization

--- midigen/models/gpt2/utils.py
+++ midigen/models/gpt2/utils.py
@@ -8,3 +8,18 @@ def generate_square_subsequent_mask(sz: int) -> Tensor:
     mask = (torch.triu(torch.ones(sz, sz)) == 1).transpose(0, 1)
     mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
     return mask
+
+class GPTConfig:
+    """ base GPT config, params common to all GPT versions """
+    embd_pdrop = 0.1
+    resid_pdrop = 0.1
+    attn_pdrop = 0.1
+
+    def __init__(self, vocab_size, block_size, dim_feedforward, enable_rpr=False, er_len=None, **kwargs):
+        self.vocab_size = vocab_size
+        self.block_size = block_size
+        self.dim_feedforward = dim_feedforward
+        self.enable_rpr = enable_rpr
+        self.er_len = er_len
+        for k,v in kwargs.items():
+            setattr(self, k, v)
