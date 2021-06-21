e4961559016b500a52abf75ea795ae2bf9184ad2
Alex <sashmark97@gmail.com>
2021-06-21 06:52:16 +0300

    Added RPR model download

--- Saved_models/download_models.sh
+++ Saved_models/download_models.sh
@@ -1,2 +1,3 @@
 wget -O gpt2_best_acc_bsize12_12epochs https://www.dropbox.com/s/uyow6mvi0rbjoge/gpt2_best_acc_bsize12_12epochs?dl=1
 wget -O gpt2_rpr_best_acc_bsize2_2epochs https://www.dropbox.com/s/7ncuw4eyk7sijg7/gpt2_rpr_best_acc_bsize2_2epochs?dl=1
+wget -O gpt2_fulltraining_rpr_moreworkers_model https://www.dropbox.com/s/zn9zxxlay1n8bix/gpt2_fulltraining_rpr_moreworkers_model?dl=1
